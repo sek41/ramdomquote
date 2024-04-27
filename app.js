8c3f98a3890809debe60af3114e283d64ed38d54
sek41 <pepin198@hotmaile.comm>
2024-04-27 03:31:39 +0200

    1.1

---
+++ app.js
@@ -0,0 +1,4 @@
+cconst button = document.getElementById('button'); // Corrected the method name from 'object' to 'getElementById'
+button.addEventListener('click', function() {
+    alert('Button clicked'); // Corrected the syntax for displaying an alert
+});
